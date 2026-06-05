81e7bfe245e1c76897e72337d4a1834c7c2fd420
talhaansari692 <talhaansari692@gmail.com>
2026-06-05 10:26:48 +0530

    fix middleware issue 2

---
+++ start.js
@@ -0,0 +1,33 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+// Set the environment to production by default if not specified
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+const port = process.env.PORT || 3000;
+
+// Initialize the Next.js application
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      // Parse the URL to pass to the Next.js handler
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
+    }
+  })
+    .once('error', (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port} (Next.js server)`);
+    });
+});
